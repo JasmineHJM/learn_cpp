53179931211b6bbd2a9e535ca3651528439681f2
JasmineHJM <965071629@qq.com>
2019-12-10 23:02:41 +0800

    提交代码

---
+++ advanced/12_10.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+//Ðéº¯Êý±í
+class Base {
+public:
+	virtual void Func1()
+	{
+		cout << "Base::Func1()" << endl;
+	}
+
+	virtual void Func2()
+	{
+		cout << "Base::Func2()" << endl;
+	}
+
+	void Func3()
+	{
+		cout << "Base::Func3()" << endl;
+	}
+
+private:    int _b = 1;
+};
+
+class Derive : public Base { 
+public:  
+	virtual void Func1()   
+	{ 
+		cout << "Derive::Func1()" << endl;
+	}
+private: 
+	int _d = 2;
+};
+
+int main() 
+{
+	Base b; 
+	Derive d;
+
+	return 0;
+}
